0aabb0fce10612cf3a95e724047c25492955aa2a
lahrd <74472177+lahrd@users.noreply.github.com>
2021-01-19 13:06:17 -0600

    Add files via upload

---
+++ directory.py
@@ -0,0 +1,34 @@
+# Script Name:
+# Author:
+# Date of last revision:
+# Description of purpose:
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+variable1 = input("Enter file Path:")
+### Read user input here into a variable
+
+# Declaration of functions
+
+### Declare a function here
+
+for (root, dirs, files) in os.walk(variable1, topdown=False):
+    ### Add a print command here to print ==root==
+    print(os.path.join(root, name))
+    ### Add a print command here to print ==dirs==
+    print(os.path.join(root, name))
+    ### Add a print command here to print ==files==
+    print(os.path.join(root, name))
+
+# Main
+
+### Pass the variable into the function here
+
+# End
+
+	
+	
+	
